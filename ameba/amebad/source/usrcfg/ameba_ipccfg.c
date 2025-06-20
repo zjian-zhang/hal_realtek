b609aba258a9e44f836cd1725a15be267d7a0550
zjian zhang <zjian_zhang@realsil.com.cn>
2025-06-20 11:11:44 +0800

    hal: realtek: add hal drivers for amebad series
    Low level files to support amebad
    
    Signed-off-by: zjian zhang <zjian_zhang@realsil.com.cn>

---
+++ ameba/amebad/source/usrcfg/ameba_ipccfg.c
@@ -0,0 +1,105 @@
+/**
+  ******************************************************************************
+  * @file    ameba_ipccfg.c
+  * @author
+  * @version V1.0.0
+  * @date    2016-05-17
+  * @brief   This file provides configurations for KM0 or KM4 IPC init
+  ******************************************************************************
+  * @attention
+  *
+  * This module is a confidential and proprietary property of RealTek and
+  * possession or use of this module requires written permission of RealTek.
+  *
+  * Copyright(c) 2015, Realtek Semiconductor Corporation. All rights reserved.
+  ******************************************************************************
+  */
+#include "ameba_soc.h"
+
+#if defined (CONFIG_ARM_CORE_CM4)
+const IPC_INIT_TABLE  ipc_init_config[] = {
+	//USER_MSG_TYPE		IRQFUNC								IRQDATA
+#ifndef __ZEPHYR__ // Zephyr does not support shell for now
+	{IPC_USER_DATA,		shell_switch_ipc_int,	(void *) NULL},//channel 0: IPC_INT_CHAN_SHELL_SWITCH
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 1: IPC_INT_CHAN_WIFI_FW
+	{IPC_USER_DATA,		FLASH_Write_IPC_Int,	(void *) NULL},//channel 2: IPC_INT_CHAN_FLASHPG_REQ
+	{IPC_USER_POINT,	NULL,	(void *) NULL},    //channel 3: IPC_INT_KM4_TICKLESS_INDICATION
+#endif
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 4: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 5: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 6: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 7: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 8: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 9: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 10: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 11: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 12: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 13: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 14: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 15: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 16: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 17: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 18: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 19: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 20: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 21: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 22: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 23: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 24: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 25: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 26: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 27: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 28: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 29: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 30: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 31: Reserved for Customer use
+
+	{0xFFFFFFFF,		OFF,								OFF},	/* Table end */
+};
+#else
+#if defined(CONFIG_WIFI_FW_EN) && CONFIG_WIFI_FW_EN
+extern void driver_fw_flow_ipc_int(void *Data, u32 IrqStatus, u32 ChanNum);
+#define fw_flow_ipc_int driver_fw_flow_ipc_int
+#else
+#define fw_flow_ipc_int NULL
+#endif
+const IPC_INIT_TABLE  ipc_init_config[] = {
+	//USER_MSG_TYPE		IRQFUNC								IRQDATA
+	{IPC_USER_DATA,		shell_switch_ipc_int,	(void *) NULL},//channel 0: IPC_INT_CHAN_SHELL_SWITCH
+	{IPC_USER_DATA,		fw_flow_ipc_int,	(void *) IPCM4_DEV},  //channel 1: IPC_INT_CHAN_WIFI_FW
+	{IPC_USER_DATA,		FLASH_Write_IPC_Int,	(void *) NULL},//channel 2: IPC_INT_CHAN_FLASHPG_REQ
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 3: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 4: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 5: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 6: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 7: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 8: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 9: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 10: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 11: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 12: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 13: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 14: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 15: Reserved for Realtek use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 16: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 17: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 18: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 19: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 20: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 21: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 22: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 23: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 24: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 25: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 26: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 27: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 28: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 29: Reserved for Customer use
+	{IPC_USER_DATA,		NULL,	(void *) NULL},    //channel 30: Reserved for Customer use
+	{IPC_USER_POINT,    km4_tickless_ipc_int,	(void *) NULL},//channel 31: IPC_INT_KM4_TICKLESS_INDICATION
+
+	{0xFFFFFFFF,		OFF,								OFF},	/* Table end */
+};
+#endif
+/******************* (C) COPYRIGHT 2016 Realtek Semiconductor *****END OF FILE****/
+
